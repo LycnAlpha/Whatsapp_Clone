f926529e50b43f9874a6d19f0c30a54a5f64357a
Shashith Dimal <shashithdperera@gmail.com>
2023-09-05 21:41:04 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/responsive/responsive_layout.dart';
+import 'package:whatsapp_clone/screens/mobileScreenLayout.dart';
+import 'package:whatsapp_clone/screens/webScreenLayout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+          mobileScreenLayout: MobileScreenLayout(),
+          webScreenLayout: WebScreenLayout()),
+    );
+  }
+}
