b0f942a60d54c1dad626e9efd848f3d4c773817e
Shashith Dimal <shashithdperera@gmail.com>
2023-09-15 00:50:08 +0530

    finished mobile interface

--- lib/screens/mobileScreenLayout.dart
+++ lib/screens/mobileScreenLayout.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/widgets/contactList.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("This is Mobile")),
-    );
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: appBarColor,
+            elevation: 0,
+            title: const Text(
+              "Whatsapp",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Colors.grey,
+                  ))
+            ],
+            bottom: TabBar(
+                indicatorColor: tabColor,
+                indicatorWeight: 4,
+                labelColor: tabColor,
+                unselectedLabelColor: Colors.grey,
+                labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                tabs: [
+                  Tab(
+                    text: "CHATS",
+                  ),
+                  Tab(
+                    text: "STATUS",
+                  ),
+                  Tab(
+                    text: "CALLS",
+                  )
+                ]),
+          ),
+          body: const ContactList(),
+        ));
   }
 }
